2008e8930fe744436d7ac9619335e96724c03d1d
Max McLaughlin <maxwelljmack@gmail.com>
2022-09-07 22:16:15 -0700

    added psuedocode

---
+++ public/assets/js/server.js
@@ -0,0 +1,12 @@
+/* 
+  set up the server in general
+  add routes
+    GET /notes = notes.html
+    GET * = index.html
+    GET /api/notes = db.json
+    POST /api/notes = post notes via postman
+      need a node module to give each note a unique ID
+  possibly some front end functionality once the backend stuff is done
+  
+  LAST- deploy to heroku
+*/
